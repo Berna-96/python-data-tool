2f3020b56090195ccb2f31dfcb44b57b5c8b055a
Berna-96 <matteobernardini8796@gmail.com>
2026-02-08 13:12:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-def calcola_media(numeri):
-    return sum(numeri) / len(numeri)
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers)
 
-def calcola_massimo(numeri):
-    massimo = numeri[0]
-    for n in numeri:
-        if n > massimo:
-            massimo = n
-    return massimo
+def calculate_max(numbers):
+    maximum = numbers[0]
+    for n in numbers:
+        if n > maximum:
+            maximum = n
+    return maximum
 
-def calcola_minimo(numeri):
-    minimo = numeri[0]
-    for n in numeri:
-        if n < minimo:
-            minimo = n
-    return minimo
+def calculate_min(numbers):
+    minimum = numbers[0]
+    for n in numbers:
+        if n < minimum:
+            minimum = n
+    return minimum
 
-numeri = [10, 5, 8, 20, 3]
+def count_numbers(data):
+    return len(data)
 
-print("Media:", calcola_media(numeri))
-print("Massimo:", calcola_massimo(numeri))
-print("Minimo:", calcola_minimo(numeri))
+numbers = [10, 5, 8, 20, 3]
+
+print("Average:", calculate_average(numbers))
+print("Maximum:", calculate_max(numbers))
+print("Minimum:", calculate_min(numbers))
+print("Total numbers:", count_numbers(numbers))
